868fa2385595d4572aeb8fb82fbfb329f2b74195
AdityaDambhare <CouncilLeader@protonmail.com>
2025-05-06 16:43:19 +0530

    fixed a bug in vm that allowed calling of non-callable values. also updated the makefile.

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -146,6 +146,7 @@ static bool call(ObjClosure* closure, int argCount) {
 
 static bool callValue(Value callee, int argCount) {
   if (IS_OBJ(callee)) {
+    printf("is obj");
     switch (OBJ_TYPE(callee)) {
       case OBJ_BOUND_METHOD: {
         ObjBoundMethod* bound = AS_BOUND_METHOD(callee);
@@ -177,9 +178,10 @@ static bool callValue(Value callee, int argCount) {
       default:
         break; // Non-callable object type.
   }
+}
   runtimeError("Can only call functions and classes.");
   return false;
-}
+
 }
 
 static bool invokeFromClass(ObjClass* klass,ObjString* method,int argCount){
